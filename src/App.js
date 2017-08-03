83d79d29698b0d5fff78708abf8c3d53ba9bbe6b
Nikita Kholin <nik.kholin@gmail.com>
2017-08-03 07:48:02 +0300

    Add error response

--- src/App.js
+++ src/App.js
@@ -6,20 +6,33 @@ const shops_api_url = 'https://discounts-ua.herokuapp.com/api/shops'
 class App extends Component {
   constructor(props) {
     super(props)
-    this.state = {}
+    this.state = {
+      requestFailed: false
+    }
   }
 
   componentDidMount() {
     fetch(shops_api_url)
+      .then(response => {
+        if (!response.ok) {
+          throw Error('Network request failed')
+        }
+        return response
+      })
       .then(data => data.json())
       .then(data => {
         this.setState({
           shops: data
         })
+      }, () => {
+        this.setState({
+          requestFailed: true
+        })
       })
   }
 
   render() {
+    if (this.state.requestFailed) return <h3>Failed!</h3>
     if (!this.state.shops) return <h3>Loading...</h3>
 
     return (
