e890649c9396e1d326f1cf9badf1c893cf8a6074
Nikita Kholin <nik.kholin@gmail.com>
2017-11-11 01:11:31 +0200

    Add time formatting

--- src/components/DiscountType.js
+++ src/components/DiscountType.js
@@ -3,11 +3,18 @@ import DiscountList from './DiscountList';
 // import './DiscountType.css';
 
 class DiscountType extends Component {
+  formatDate(date) {
+    return (new Date(date)).toLocaleDateString()
+  }
+
   render() {
+    const startDate = this.formatDate(this.props.startDate)
+    const endDate = this.formatDate(this.props.endDate)
+
     return (
       <div className="DiscountType">
         <div className="DiscountTypeInfo">
-          {this.props.name} (from {this.props.startDate} to {this.props.endDate})
+          {this.props.name} (from {startDate} to {endDate})
         </div>
         <DiscountList discounts={this.props.discounts} />
       </div>
