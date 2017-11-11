67b7718bd9965a15942d3e30d5ef1ba3c1aa6c43
Nikita Kholin <nik.kholin@gmail.com>
2017-11-12 00:44:29 +0200

    Create API class and use it in routes

--- src/components/App.js
+++ src/components/App.js
@@ -1,6 +1,7 @@
 import React, { Component } from 'react';
 import './App.css';
 import { Switch, Route, Link } from 'react-router-dom'
+import API from '../utils/api';
 import Shop from './Shop';
 import Loading from './Loading';
 import Failed from './Failed';
@@ -15,39 +16,11 @@ class App extends Component {
   }
 
   componentDidMount() {
-    // const shops_api_url = 'https://discounts-ua.herokuapp.com/api/shops'
-    // 
-    // fetch(shops_api_url)
-    //   .then(response => {
-    //     if (!response.ok) {
-    //       throw Error('Network request failed')
-    //     }
-    //     return response
-    //   })
-    //   .then(data => data.json())
-    //   .then(data => {
-    //     this.setState({
-    //       shops: data
-    //     })
-    //   }, () => {
-    //     this.setState({
-    //       requestFailed: true
-    //     })
-    //   })
-    this.setState({
-      "shops": [
-        {
-          "id": 1,
-          "name":"ATB",
-          "path":"atb"
-        },
-        {
-          "id": 2,
-          "name":"Silpo",
-          "path":"silpo"
-        }
-      ],
-      activeShopId: 1
+    API.shops().then(shops => {
+      this.setState({
+        "shops": shops,
+        activeShopId: 1
+      })
     })
   }
 
@@ -55,14 +28,11 @@ class App extends Component {
     if (this.state.requestFailed) return <Failed />
     if (!this.state.shops) return <Loading />
 
-    const links = this.state.shops.map((shop) =>
+    const links = this.state.shops.map(shop =>
         <Link to={shop.path}
-              className={"MenuShop" + (shop.id == this.state.activeShopId ? " MenuShopActive" : "")}>
+              className={"MenuShop" + (shop.id === this.state.activeShopId ? " MenuShopActive" : "")}>
           {shop.name}
         </Link>
-    )
-    const routes = this.state.shops.map((shop) =>
-      <Route path={`/${shop.path}/`} component={Shop}/>
     )
 
     return (
@@ -72,7 +42,9 @@ class App extends Component {
           <div className="MenuShopNames">{links}</div>
         </div>
 
-        <Switch>{routes}</Switch>
+        <Switch>
+          <Route path={"/:shop"} component={Shop}/>
+        </Switch>
       </div>
     );
   }
