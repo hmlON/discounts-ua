b05c9eee0ff1e9938fb4ca571c7caaa0472bff78
Nikita Kholin <nik.kholin@gmail.com>
2018-06-16 13:55:49 +0300

    Add react-testing-library and fix App test

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -1,18 +1,20 @@
 import React, { Component } from 'react';
 import './App.css';
-import { Switch, Route } from 'react-router-dom'
+import { BrowserRouter, Switch, Route } from 'react-router-dom'
 import Shop from '../Shop/Shop';
 import Menu from '../Menu/Menu';
 
 class App extends Component {
   render() {
     return (
-      <div className="App">
-        <Switch>
-          <Route path={"/discounts-ua/:shop"} component={Shop}/>
-          <Route path={"/discounts-ua"} component={Menu}/>
-        </Switch>
-      </div>
+      <BrowserRouter>
+        <div className="App">
+          <Switch>
+            <Route path={"/discounts-ua/:shop"} component={Shop}/>
+            <Route path={"/discounts-ua"} component={Menu}/>
+          </Switch>
+        </div>
+      </BrowserRouter>
     );
   }
 }
--- src/index.js
+++ src/index.js
@@ -3,12 +3,9 @@ import ReactDOM from 'react-dom';
 import 'normalize.css';
 import './index.css';
 import App from './components/App/App';
-import { BrowserRouter } from 'react-router-dom'
 import registerServiceWorker from './registerServiceWorker';
 
 ReactDOM.render((
-  <BrowserRouter>
-    <App />
-  </BrowserRouter>
+  <App />
 ), document.getElementById('root'));
 registerServiceWorker();
